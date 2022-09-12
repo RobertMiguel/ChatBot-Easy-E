45ee8c401ff07fa47ab56c8e6ac2b28b02a93ea4
RobertMiguel <robertresende9@gmail.com>
2022-09-11 23:38:29 -0300

    chat

--- Easy-E/cadastro.php
+++ Easy-E/cadastro.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Eazy-E | Login</title>
+    <title>Eazy-E | Cadastro</title>
     <link rel="apple-touch-icon" sizes="180x180" href="./apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="./favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="./favicon-16x16.png">
@@ -13,19 +13,30 @@
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <div class="content">
-        <div class="text">Cadastro</div>
-        <form method="POST">
-            <div class="field">
-                <span class="fas fa-user"></span>
-                <input type="text" name="email" placeholder="Digite o e-mail">
+	<div class="main-login">
+        <div class="left-login">
+            <h1>Faça o cadastro<br> e venha fazer parte disso !</h1>
+            <img src="Game analytics-amico.svg" alt="" class="image-login">
+        </div>
+        <div class="rigth-login">
+            <div class="card-login">
+                <h1>LOGIN</h1>
+				<form method="post">
+					<div class="textfield">
+						<input type="text" name="usuario" placeholder="Digite o e-mail">
+					</div>
+					<div class="textfield">
+						<input type="text" name="nome" placeholder="Digite seu nome">
+					</div>
+					<div class="textfield">
+						<input type="password" name="senha" placeholder="Digite a senha">
+					</div>
+				</form>
+				<input type="submit" value="Criar Conta" class="btn-login">
+
             </div>
-            <div class="field">
-                <span class="fas fa-lock"></span>
-                <input type="password" name="senha" placeholder="Digite a senha">
-            </div>
-            <input type="submit" value="Criar conta">
-        </form>
+        </div>
+
     </div>
 </body>
 </html>
